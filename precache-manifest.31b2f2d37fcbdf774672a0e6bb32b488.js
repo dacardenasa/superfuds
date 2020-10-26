3c4232d1d95145a13a62021095d27b30d0ac0199
dacardenasa <diego.cardenasaleg@gmail.com>
2020-10-25 21:32:08 -0500

    Updates

---
+++ precache-manifest.31b2f2d37fcbdf774672a0e6bb32b488.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfcdce3ae97bd1a18b7a1fc0f0ca75a4",
+    "url": "/superfuds/index.html"
+  },
+  {
+    "revision": "495fc135ec573eb0a428",
+    "url": "/superfuds/static/css/2.1cd5c093.chunk.css"
+  },
+  {
+    "revision": "f55a999fd63204c58629",
+    "url": "/superfuds/static/css/main.1edfd029.chunk.css"
+  },
+  {
+    "revision": "495fc135ec573eb0a428",
+    "url": "/superfuds/static/js/2.ec8ea2c3.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/superfuds/static/js/2.ec8ea2c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55a999fd63204c58629",
+    "url": "/superfuds/static/js/main.6f1b582e.chunk.js"
+  },
+  {
+    "revision": "36298fc22f95b73c3963",
+    "url": "/superfuds/static/js/runtime-main.47935a93.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/superfuds/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/superfuds/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "84cb5ac5e19b614016e4ba3f424e44b5",
+    "url": "/superfuds/static/media/logo.84cb5ac5.svg"
+  },
+  {
+    "revision": "8039a005a08fc14e3f74e75d70cfbdb1",
+    "url": "/superfuds/static/media/profile.8039a005.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/superfuds/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/superfuds/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/superfuds/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/superfuds/static/media/slick.f97e3bbf.svg"
+  }
+]);
